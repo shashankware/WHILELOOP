ccdd1ce6c39e5f339284dfbc20deea0e735b0b1a
shashank <shashank.w1998@gmail.com>
2021-02-17 23:22:09 +0530

    headtail added

--- whilepow.sh
+++ whilepow.sh
@@ -42,3 +42,20 @@ do
    fi
 done
 
+#!/bin/bash
+head=1
+tail=0
+countHead=0
+counTails=0
+while [ $countHead -lt "11" ]
+do
+   randomNumber=$(($RANDOM%2))
+   if [ $randomNumber -eq "1" ]
+   then
+      countHead=$(($countHead+1))
+      echo "Head wins(count):" $countHead
+   else
+      echo "Tail wins"
+   fi
+done
+
