16c1ccd66af449ee17dadf409f82f60fd47c3347
shashank <shashank.w1998@gmail.com>
2021-02-17 20:55:36 +0530

    magic added

--- whilepow.sh
+++ whilepow.sh
@@ -14,3 +14,31 @@ echo "value=" $mul
 i=$(($i+1))
 done
 
+#!/bin/bash
+ read -p "Think Number(1-10) : " N
+start=1
+end=10
+temp2=0
+yes=1
+no=0
+while [ $temp2 -ne $N ]
+do
+   temp1=$(($start+$end))
+   temp2=$(($temp1/2))
+   echo "is it number $temp2(yes 1/no 0):"
+   read ans
+   if [ $ans -eq $yes ]
+   then
+      echo "Found..."
+   else
+      echo "Greater or smaller than  5(1 G/0 S): "
+      read ans1
+      if [ $ans1 -eq $yes ]
+      then
+         start=$temp2
+      else
+         end=$temp2
+      fi
+   fi
+done
+
