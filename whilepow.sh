ad12b9d8812fa4eb428639ae51df72364157a0e4
shashank <shashank.w1998@gmail.com>
2021-02-17 23:25:46 +0530

    gambler added

--- whilepow.sh
+++ whilepow.sh
@@ -59,3 +59,25 @@ do
    fi
 done
 
+#!/bin/bash
+cash=100
+goal=200
+count=0
+bet=1
+win=1
+loose=0
+while [ $cash -gt $bet ] && [ $cash -lt $goal ]
+do
+   randomNumber=$(($RANDOM%2))
+   if [ $randomNumber -eq "1" ]
+   then
+      cash=$(($cash+10))
+      count=$(($count+1))
+      echo "Gambler Wins" $count
+   else
+      echo "Gambler Loose"
+   fi
+
+done
+
+
